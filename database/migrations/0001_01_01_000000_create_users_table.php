4f80daa496b3fa560dd9ad1e7891b21d3208b85d
SC-Elizabeth <56262076+Eamead@users.noreply.github.com>
2024-10-08 18:07:17 -0400

    Update User Migration
    + Add Username Column.
    + Add Role Enum Column to distinguish between admins and standard users.
    + Add Status Column to check if a resource has been approved yet.

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username', 50)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('status', ['pending', 'approved'])->default('pending');
             $table->rememberToken();
             $table->timestamps();
         });
